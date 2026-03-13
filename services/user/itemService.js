8b8eda916d6e109af36cdeb880fd628d8d1516d7
AkhilBhanderi <akhilbhanderi7@gmail.com>
2026-03-13 15:33:26 +0530

    add user mobile number in get all item api

--- services/user/itemService.js
+++ services/user/itemService.js
@@ -11,6 +11,125 @@ module.exports = {
       throw createError.InternalServerError(error);
     }
   },
+  // getAllItems: async (
+  //   deal_type,
+  //   price_sort,
+  //   pet_sort,
+  //   date_sort,
+  //   property_type,
+  //   budget,
+  //   search,
+  //   pagenumber = 1,
+  //   limit = 10,
+  //   user_id,
+  // ) => {
+  //   try {
+  //     const offset = (pagenumber - 1) * limit;
+
+  //     const whereCondition = {
+  //       active: true,
+  //       // ...(deal_type && { deal_type }),
+  //       ...(user_id && { user_id }),
+  //     };
+
+  //     // Deal Type Filter
+  //     if (deal_type) {
+  //       whereCondition.deal_type = deal_type;
+  //     }
+
+  //     if (property_type) {
+  //       whereCondition.property_type = property_type;
+  //     }
+
+  //     // Budget Filter
+  //     if (budget) {
+  //       whereCondition.main_price = {
+  //         [Op.lte]: budget,
+  //       };
+  //     }
+
+  //     // Search Filter
+  //     if (search) {
+  //       whereCondition[Op.or] = [
+  //         { description_1: { [Op.iLike]: `%${search}%` } },
+  //         { property_area_name: { [Op.iLike]: `%${search}%` } },
+  //         { pincode: { [Op.iLike]: `%${search}%` } },
+  //         { country: { [Op.iLike]: `%${search}%` } },
+  //         { state: { [Op.iLike]: `%${search}%` } },
+  //         { city: { [Op.iLike]: `%${search}%` } },
+  //       ];
+  //     }
+
+  //     let order = [];
+
+  //     // Newest / Oldest filter
+  //     if (date_sort) {
+  //       order.push(["createdAt", date_sort === "new" ? "DESC" : "ASC"]);
+  //     }
+
+  //     if (price_sort) {
+  //       order.push(["main_price", price_sort === "lth" ? "ASC" : "DESC"]);
+  //     }
+
+  //     if (pet_sort) {
+  //       order.push(["pet_type", pet_sort === "sqlth" ? "ASC" : "DESC"]);
+  //     }
+
+  //     const { rows: itemData } = await items.findAndCountAll({
+  //       where: whereCondition,
+  //       limit,
+  //       offset,
+  //       order,
+  //       attributes: { exclude: ["updatedAt"] },
+  //       include: [
+  //         {
+  //           model: users,
+  //           as: "user",
+  //           attributes: ["mobile_no"],
+  //         },
+  //         {
+  //           model: auctions,
+  //           as: "auctions",
+  //           required: false,
+  //           attributes: [
+  //             "id",
+  //             "item_id",
+  //             "user_id",
+  //             // "description",
+  //             "price",
+  //             "owner_name",
+  //             "createdAt",
+  //             "updatedAt",
+  //           ],
+  //           include: [
+  //             {
+  //               model: users,
+  //               as: "user",
+  //               attributes: ["id", "mobile_no", "otp", "active", "fcm_token"],
+  //             },
+  //           ],
+  //         },
+  //       ],
+  //     });
+
+  //     // Map auctions to the auction field
+  //     const formattedItems = itemData.map((item) => {
+  //       const itemJSON = item.toJSON();
+
+  //       return {
+  //         ...itemJSON,
+  //         // auction: itemJSON.auctions.length
+  //         //   ? itemJSON.auctions.map((a) => a.id) // you can store ids or latest auction id
+  //         //   : null, // if no auction
+  //       };
+  //     });
+
+  //     return { itemData: formattedItems };
+  //   } catch (error) {
+  //     throw createError.InternalServerError(error.message);
+  //   }
+  // },
+
   getAllItems: async (
     deal_type,
     price_sort,
@@ -28,7 +147,6 @@ module.exports = {
 
       const whereCondition = {
         active: true,
-        // ...(deal_type && { deal_type }),
         ...(user_id && { user_id }),
       };
 
@@ -37,6 +155,7 @@ module.exports = {
         whereCondition.deal_type = deal_type;
       }
 
+      // Property Type Filter
       if (property_type) {
         whereCondition.property_type = property_type;
       }
@@ -62,15 +181,17 @@ module.exports = {
 
       let order = [];
 
-      // Newest / Oldest filter
+      // Date Sort
       if (date_sort) {
         order.push(["createdAt", date_sort === "new" ? "DESC" : "ASC"]);
       }
 
+      // Price Sort
       if (price_sort) {
         order.push(["main_price", price_sort === "lth" ? "ASC" : "DESC"]);
       }
 
+      // Pet Sort
       if (pet_sort) {
         order.push(["pet_type", pet_sort === "sqlth" ? "ASC" : "DESC"]);
       }
@@ -83,6 +204,11 @@ module.exports = {
         attributes: { exclude: ["updatedAt"] },
         include: [
           {
+            model: users,
+            as: "user",
+            attributes: ["mobile_no"],
+          },
+          {
             model: auctions,
             as: "auctions",
             required: false,
@@ -90,7 +216,6 @@ module.exports = {
               "id",
               "item_id",
               "user_id",
-              // "description",
               "price",
               "owner_name",
               "createdAt",
@@ -107,15 +232,15 @@ module.exports = {
         ],
       });
 
-      // Map auctions to the auction field
+      // Format response (move mobile_no outside user)
       const formattedItems = itemData.map((item) => {
         const itemJSON = item.toJSON();
 
+        const { user, ...rest } = itemJSON;
+
         return {
-          ...itemJSON,
-          // auction: itemJSON.auctions.length
-          //   ? itemJSON.auctions.map((a) => a.id) // you can store ids or latest auction id
-          //   : null, // if no auction
+          ...rest,
+          mobile_no: user?.mobile_no || null,
         };
       });
 
